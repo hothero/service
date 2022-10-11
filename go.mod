88a8b72b1b4b775337591197f2e1fc9c773cd4aa
Ronald Hsu (hothero) <hothero0705@gmail.com>
2022-10-12 00:00:59 +0800

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/hothero/service
+
+go 1.16
+
+require github.com/hothero/generated-protos v0.0.0-20221011154450-41e4c242c4d4
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"github.com/hothero/generated-protos/listing"
+)
+
+func main() {
+	l := new(listing.Listing)
+	fmt.Println(l)
+
+	return
+}
